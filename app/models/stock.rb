2c514325f905e4be93ca946ed2149a33b60dbe03
giorgi01 <giorgi.tamarashvili.1@btu.edu.ge>
2020-02-02 20:03:03 +0400

    Heroku url changed

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -10,7 +10,7 @@ class Stock < ApplicationRecord
 
   def self.new_lookup(ticker_symbol)
     client = IEX::Api::Client.new(
-        publishable_token: 'API_KEY',
+        publishable_token: 'Tpk_2551ad26afe543afaf3c10c9f6f2fe94',
         endpoint: 'https://sandbox.iexapis.com/v1')
     begin
       new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
