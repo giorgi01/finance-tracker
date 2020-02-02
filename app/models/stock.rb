fc11e0ecbd71de6bc7dc29c505ac49176116eaa4
giorgi01 <giorgi.tamarashvili.1@btu.edu.ge>
2020-02-02 14:53:10 +0400

    Added StockController,Views

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -2,8 +2,8 @@ class Stock < ApplicationRecord
 
   def self.new_lookup(ticker_symbol)
     client = IEX::Api::Client.new(
-        publishable_token: 'YOUR_API_KEY',
+        publishable_token: 'API_KEY',
         endpoint: 'https://sandbox.iexapis.com/v1')
-    client.price(ticker_symbol)
+    new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
   end
 end
