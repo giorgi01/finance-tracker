47488c44669c309f52d6c51d76980d2e8f39f7a8
giorgi01 <giorgi.tamarashvili.1@btu.edu.ge>
2020-02-04 00:06:34 +0400

    Added tracking restrictions & tracking remove functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,18 @@ class User < ApplicationRecord
   has_many :stocks, through: :user_stocks
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def under_stock_limit?
+    stocks.count < 10
+  end
+
+  def stock_already_tracked?(ticker_symbol)
+    stock = Stock.check_db(ticker_symbol)
+    return false unless stock
+    Stock.where(id: stock.id).exists? && stocks.include?(stock)
+  end
+
+  def can_track_stock?(ticker_symbol)
+    under_stock_limit? && !stock_already_tracked?(ticker_symbol)
+  end
 end
