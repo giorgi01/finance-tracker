9ff374c40bff5fa7eddc31d10e9bd6c6033f8fec
giorgi01 <giorgi.tamarashvili.1@btu.edu.ge>
2020-02-06 00:21:37 +0400

    Added Friendships, Search, Add/Remove Friend

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   resources :user_stocks, only: [:create, :destroy]
-  devise_for :users
+  devise_for :users, controllers: { registrations: "user/registrations" }
   root 'welcome#index'
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_stock', to: 'stocks#search'
+  resources :users, only: [:show]
+  resources :friendships
+  get 'my_friends', to: 'users#my_friends'
+  get 'search_friends', to: 'users#search'
+  post 'add_friend', to: "users#add_friend"
 end
